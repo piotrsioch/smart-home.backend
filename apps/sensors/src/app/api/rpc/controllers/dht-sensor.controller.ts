00ece51b1bfaca2954863b3a01449c0e66b5929a
piotrsioch <windyswire@gmail.com>
2023-12-03 20:31:39 +0100

    Add dht data works through api-gateway

--- apps/sensors/src/app/api/rpc/controllers/dht-sensor.controller.ts
+++ apps/sensors/src/app/api/rpc/controllers/dht-sensor.controller.ts
@@ -6,15 +6,22 @@ import {
   DhtSensorDto,
   SensorsCommunicationEnum,
 } from '@smart-home.backend/libs/common';
+import { AddDhtSensorDataCommand } from "../../../application";
 
 @Controller()
 export class DhtSensorController {
   constructor(private readonly commandBus: CommandBus) {}
 
   @MessagePattern(SensorsCommunicationEnum.ADD_DHT_DATA)
-  async addDhtData(@Payload() payload: AddDhtSensorDataInputDto): Promise<any> {
-    //dhtsensordto
-    console.log(payload);
+  async addDhtData(@Payload() payload: AddDhtSensorDataInputDto): Promise<DhtSensorDto> {
     const { temperature, sensorId, humidity } = payload;
+
+    const command = new AddDhtSensorDataCommand({
+      temperature,
+      sensorId,
+      humidity,
+    });
+
+    return await this.commandBus.execute<AddDhtSensorDataCommand>(command);
   }
 }
