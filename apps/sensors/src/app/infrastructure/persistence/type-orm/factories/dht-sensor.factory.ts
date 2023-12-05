65bd102ef747cadde51d233e8bb655f43a704ac0
piotrsioch <windyswire@gmail.com>
2023-12-05 17:42:18 +0100

    Invoke sensor.create in sensor factory so entity has proper id in mongo

--- apps/sensors/src/app/infrastructure/persistence/type-orm/factories/dht-sensor.factory.ts
+++ apps/sensors/src/app/infrastructure/persistence/type-orm/factories/dht-sensor.factory.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { DhtSensorEntity } from '../entities/dht-sensor.entity';
 import { faker } from '@faker-js/faker';
+import { DhtSensor } from '../../../../domain/models';
 
 @Injectable()
 export class DhtSensorFactory {
   create(): DhtSensorEntity {
-    const sensor = new DhtSensorEntity();
+    const sensor = new DhtSensor();
+    sensor.create();
 
     sensor.temperature = faker.number.int({ min: -30, max: 60 }).toString();
     sensor.humidity = faker.number.int({ min: 0, max: 100 }).toString();
