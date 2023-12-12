58e8ed26faf8b99fc4044ecef43ea06c6d0a257a
piotrsioch <windyswire@gmail.com>
2023-12-12 18:20:04 +0100

    Adjust messages sent during pir-sensor and smoke-sensor events.

--- libs/common/src/dto/sensors/events/smoke-sensor-critical-value-detected.event.dto.ts
+++ libs/common/src/dto/sensors/events/smoke-sensor-critical-value-detected.event.dto.ts
@@ -1,1 +1,6 @@
-export class SmokeSensorCriticalValueDetectedEventDto {}
+import { IsString } from 'class-validator';
+
+export class SmokeSensorCriticalValueDetectedEventDto {
+  @IsString()
+  sensorId: string;
+}
