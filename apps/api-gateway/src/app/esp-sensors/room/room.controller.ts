79b213b43861394c0e7613168935019dd091d147
piotrsioch <windyswire@gmail.com>
2023-12-20 16:36:19 +0100

    Add api layer for room commands

--- apps/api-gateway/src/app/esp-sensors/room/room.controller.ts
+++ apps/api-gateway/src/app/esp-sensors/room/room.controller.ts
@@ -1,15 +1,18 @@
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Query, UseFilters } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseFilters } from '@nestjs/common';
 import {
   ApiOkResponsePaginated,
+  CreateRoomInputDto,
   CustomClientProxy,
   CustomExceptionFilter,
   IdInputDto,
   PaginationOutput,
   RoomDto,
   RoomListInputDto,
+  RoomSensorInputDto,
   SensorsCommunicationEnum,
   ServiceEnum,
+  SuccessDto,
 } from '@smart-home.backend/libs/common';
 
 @ApiTags('Room')
@@ -32,6 +35,42 @@ export class RoomController {
   async getLatestData(@Query() input: IdInputDto): Promise<RoomDto> {
     return await this.client.sendTo(ServiceEnum.SENSORS, {
       pattern: SensorsCommunicationEnum.GET_ROOM_BY_ID,
+      data: input,
+    });
+  }
+
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Post('/create-room')
+  async createRoom(@Body() input: CreateRoomInputDto): Promise<RoomDto> {
+    return await this.client.sendTo(ServiceEnum.SENSORS, {
+      pattern: SensorsCommunicationEnum.CREATE_ROOM,
+      data: input,
+    });
+  }
+
+  @ApiResponse({ status: 200, type: SuccessDto })
+  @Post('/delete-room')
+  async deleteRoom(@Body() input: IdInputDto): Promise<SuccessDto> {
+    return await this.client.sendTo(ServiceEnum.SENSORS, {
+      pattern: SensorsCommunicationEnum.DELETE_ROOM,
+      data: input,
+    });
+  }
+
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Post('/assign-sensor-to-room')
+  async assignSensorToRoom(@Body() input: RoomSensorInputDto): Promise<RoomDto> {
+    return await this.client.sendTo(ServiceEnum.SENSORS, {
+      pattern: SensorsCommunicationEnum.ASSIGN_SENSOR_TO_ROOM,
+      data: input,
+    });
+  }
+
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Post('/remove-sensor-from-room')
+  async removeSensorFromRoom(@Body() input: RoomSensorInputDto): Promise<RoomDto> {
+    return await this.client.sendTo(ServiceEnum.SENSORS, {
+      pattern: SensorsCommunicationEnum.CREATE_ROOM,
       data: input,
     });
   }
--- libs/common/src/dto/sensors/input/room/index.ts
+++ libs/common/src/dto/sensors/input/room/index.ts
@@ -1,2 +1,3 @@
 export * from './create-room.input.dto';
 export * from './room-list-input.dto';
+export * from './room-sensor.input.dto';
